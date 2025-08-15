2b2585f3957613d624b421a6fb821c811f027a5c
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-08-15 21:39:37 +0300

    add difficulty and category features

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -11,7 +11,33 @@ export default function IntroPage() {
       ) : (
         <div className="introDiv jcontent">
           <h1>Quizzical</h1>
-          <p>Just Answer to this 7 Questions (Sports Categoty !!)</p>
+          <p>Simply cheif your short quiz with your fav Category</p>
+          <div className="cat-section">
+            <div>
+              <label for="category">Category : </label>
+              <select name="category">
+                <option>Sports</option>
+                <option>Comics</option>
+                <option>Film</option>
+                <option>Art</option>
+                <option>General</option>
+                <option>Books</option>
+                <option>Computers</option>
+                <option>History</option>
+                <option>Anime & Manga</option>
+                <option>Cartoon & Animations</option>
+                <option>Geography</option>
+              </select>
+            </div>{" "}
+            <div>
+              <label for="difficulty">Select Difficulty : </label>
+              <select name="difficulty">
+                <option>Easy</option>
+                <option>Medium</option>
+                <option>Hard</option>
+              </select>
+            </div>
+          </div>
           <button onClick={() => setStartQuiz(() => true)}>Start Quiz</button>
         </div>
       )}
