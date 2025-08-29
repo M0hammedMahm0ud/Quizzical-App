0d36a98ea895900aa02eb6ea270c7407cae494cc
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-08-29 16:40:30 +0300

    more clean

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -12,52 +12,6 @@ export default function IntroPage() {
     catId: "Sports",
     num: 10,
   });
-  // const categories = [
-  //   {
-  //     category: "Sports",
-  //     apiId: 21,
-  //   },
-  //   {
-  //     category: "Comics",
-  //     apiId: 29,
-  //   },
-  //   {
-  //     category: "Film",
-  //     apiId: 11,
-  //   },
-  //   {
-  //     category: "Art",
-  //     apiId: 25,
-  //   },
-  //   {
-  //     category: "General",
-  //     apiId: 9,
-  //   },
-  //   {
-  //     category: "Books",
-  //     apiId: 10,
-  //   },
-  //   {
-  //     category: "Computers",
-  //     apiId: 18,
-  //   },
-  //   {
-  //     category: "History",
-  //     apiId: 23,
-  //   },
-  //   {
-  //     category: "Anime & Manga",
-  //     apiId: 31,
-  //   },
-  //   {
-  //     category: "Cartoon & Animations",
-  //     apiId: 32,
-  //   },
-  //   {
-  //     category: "Geography",
-  //     apiId: 22,
-  //   },
-  // ];
   const difficulty = ["easy", "medium", "hard"];
   return (
     <>
